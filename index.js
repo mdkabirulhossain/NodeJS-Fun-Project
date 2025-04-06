cf5dbbe2f2a51197fc6fd8207dfddeefba83c54c
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-04-06 23:20:07 +0600

    Server is created

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res)=>{
+    res.writeHead(200, {'content-type':'text/html'});
+    res.write("welcome to the server!");
+    res.end();
+})
+
+server.listen(3000);
+console.log('Server is running on prot 3000')
