694dec7d7c99af825a7177204a11c811597cefa5
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-04-06 23:37:50 +0600

    Four page is created

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ const server = http.createServer((req, res)=>{
     res.end();
 })
 
+//When we hosting website then we use port number 8080
 server.listen(3000);
 console.log('Server is running on prot 3000')
