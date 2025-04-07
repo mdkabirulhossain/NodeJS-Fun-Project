ae1b8c5f16543926c2ce718a6b084c6dcc8ccbb3
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-04-07 22:37:52 +0600

    Home page all the link are updated

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url')
 
 const server = http.createServer((req, res)=>{
-    res.writeHead(200, {'content-type':'text/html'});
-    res.write("welcome to the server!");
-    res.end();
+    if(req.url == '/'){
+        fs.readFile('Home.html', (err, data)=>{
+            if(!err){
+                res.writeHead(200, {'content-type': 'text/html'});
+                res.write(data);
+                res.end();
+            }else{
+                res.write("File not found");
+                res.end();
+            }
+        })
+    }
+    else if(req.url == '/about'){
+        fs.readFile('About.html', (err, data)=>{
+            if(!err){
+                res.writeHead(200, {'content-type': 'text/html'});
+                res.write(data);
+                res.end();
+            }else{
+                res.write("File not found");
+                res.end();
+            }
+        })
+    }
+    
 })
 
 //When we hosting website then we use port number 8080
